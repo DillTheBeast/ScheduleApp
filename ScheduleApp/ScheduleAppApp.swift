932b91be1d695b23018f6a42f449dc7c0cbf33d8
DillTheBeast <dillonmaltese@gmail.com>
2023-10-19 09:10:30 -0400

    w

---
+++ ScheduleApp/ScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleAppApp.swift
+//  ScheduleApp
+//
+//  Created by Dillon Maltese on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
